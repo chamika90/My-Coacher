8a68d60a9d0870932362e494e9fd5bb1957645f7
chamika <chamika.hasanthi@axiatadigitallabs.com>
2021-10-25 17:19:56 +0530

    Move android permission check to utils

--- src/helper/utils.ts
+++ src/helper/utils.ts
@@ -1,3 +1,5 @@
+import {PermissionsAndroid} from 'react-native';
+
 export const toBase64 = (fileName: string, filePath: string) =>
   new Promise((resolve, reject) => {
     var file = new File([fileName], filePath);
@@ -12,3 +14,33 @@ export const toBase64 = (fileName: string, filePath: string) =>
     // Convert data to base64
     reader.readAsDataURL(file);
   });
+
+export const checkPermission = async () => {
+  try {
+    const grants = await PermissionsAndroid.requestMultiple([
+      PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
+      PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE,
+      PermissionsAndroid.PERMISSIONS.RECORD_AUDIO,
+    ]);
+
+    console.log('write external stroage', grants);
+
+    if (
+      grants['android.permission.WRITE_EXTERNAL_STORAGE'] ===
+        PermissionsAndroid.RESULTS.GRANTED &&
+      grants['android.permission.READ_EXTERNAL_STORAGE'] ===
+        PermissionsAndroid.RESULTS.GRANTED &&
+      grants['android.permission.RECORD_AUDIO'] ===
+        PermissionsAndroid.RESULTS.GRANTED
+    ) {
+      console.log('Permissions granted');
+      return true;
+    } else {
+      console.log('All required permissions not granted');
+      return false;
+    }
+  } catch (err) {
+    console.warn(err);
+    return false;
+  }
+};
